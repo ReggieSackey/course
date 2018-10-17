97d102ca4a0a7d8782f877b54d040457aa5aee0f
ReggieSackey <sackeyad@grinnell.edu>
2018-10-16 23:44:14 -0500

    Learning

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_203531) do
+ActiveRecord::Schema.define(version: 2018_10_17_043906) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
