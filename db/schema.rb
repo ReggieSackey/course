8a6af7814e6246b06e61b2a82980ff6eb5779f8b
ReggieSackey <sackeyad@grinnell.edu>
2018-10-22 18:32:08 -0500

    Important stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_043906) do
+ActiveRecord::Schema.define(version: 2018_10_22_232245) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
